41eed48a9cae338decce983cac6f46415642ba77
Rakesh Raushan <rakesh@coffeebeans.io>
2019-11-06 15:36:25 +0530

    rename

---
+++ fixed_xor.py
@@ -0,0 +1,7 @@
+def calculate_xor(input_string):
+    decoded_hex =
+
+
+if __name__ == '__main__':
+    input_str = "1c0111001f010100061a024b53535009181c"
+    calculate_xor(input_str)
--- hex_to_base64.py
+++ hex_to_base64.py
@@ -1,13 +1,13 @@
 import codecs
 
 
-def convert_hex_to_base64():
-    bytes_data = codecs.decode(hex_str, 'hex')
+def convert_hex_to_base64(hex_data):
+    bytes_data = codecs.decode(hex_data, 'hex')
     print(type(bytes_data))
     print(codecs.encode(bytes_data, 'base64').decode())
 
 
 if __name__ == '__main__':
     hex_str = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
-    base64_str = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
-    convert_hex_to_base64()
+    # Output base64_str = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
+    convert_hex_to_base64(hex_str)
