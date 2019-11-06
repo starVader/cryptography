b64ab8e290c108e2256ba0909bdc87524065b43f
Rakesh Raushan <rakesh@coffeebeans.io>
2019-11-06 16:33:29 +0530

    add structure for chalenge 3

---
+++ single_byte_xor_cipher.py
@@ -0,0 +1,10 @@
+from fixed_xor import calculate_xor
+
+
+def single_byte_xor_cipher(output):
+    alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+                 'u', 'v', 'w', 'x', 'y', 'z']
+
+    if __name__ == '__main__':
+        hex_enc_xored_str = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
+        single_byte_xor_cipher(hex_enc_xored_str)
